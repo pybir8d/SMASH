11b456c90a01f70f5cc3cc01ac7c22c5f919dc8a
livia <liviag.earl@gmail.com>
2022-12-05 12:00:32 -0500

    Added code to the conversion but did not test it

--- convert-to-hls.py
+++ convert-to-hls.py
@@ -1,6 +1,6 @@
 
 import ffmpeg
-input = ffmpeg.input('in.mp4')
-audio = input.audio.filter("aecho", 0.8, 0.9, 1000, 0.3)
-video = input.video.hflip()
-out = ffmpeg.output(audio, video, 'out.mp4')
+
+input_stream = ffmpeg.input('input.mp4', f='mp4')
+output_stream = ffmpeg.output(input_stream, 'output.m3u8', format='hls', start_number=0, hls_time=5, hls_list_size=0)
+ffmpeg.run(output_stream)
