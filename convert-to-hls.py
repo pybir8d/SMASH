315873854b50de2dfcc87c06ba5c3445285503e3
livia <liviag.earl@gmail.com>
2022-12-05 13:52:53 -0500

    Added code to the conversion but did not test it

--- convert-to-hls.py
+++ convert-to-hls.py
@@ -1,6 +1,6 @@
 
 import ffmpeg
-
-input_stream = ffmpeg.input('input.mp4', f='mp4')
+file = input("Enter file: ")
+input_stream = ffmpeg.input(file, f='mp4')
 output_stream = ffmpeg.output(input_stream, 'output.m3u8', format='hls', start_number=0, hls_time=5, hls_list_size=0)
 ffmpeg.run(output_stream)
