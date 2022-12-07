d378b218c02ba227b291960c070706051e327a60
livia <liviag.earl@gmail.com>
2022-12-07 13:52:01 -0500

    Added more click options for conversion

--- Smash.py
+++ Smash.py
@@ -7,13 +7,19 @@ import click
 
 @click.command()
 @click.option("--test", "-t", default = 1, help = "Test Value")
+@click.option("--inFile", "-i", default = 'out.mp4', help = "mp4 video file inputted by the user")
+@click.option("--outFile", "-o", default = 'output.m3u8', help = "Name of the export file")
 
 def testVariable(test):
     #for test variables
     test_var = test
     click.echo("I'm doing something 1. This is test variable: " + str(test_var))
 
-def main(argv):
+def fileConvert(inFile, outFile):
+    #for file conversion
+    temp = inFile
+
+def main():
     """
     This function is the entry point for this application.
 
@@ -21,61 +27,6 @@ def main(argv):
     """
     # Program Variables
     test_var = 0
-"""
-    ####################################
-    ### Command Line Processing
-    ####################################
-    try:
-        # Convert the list of user provided command line arguments into meaningful objects to manipulate.
-        # The attributes as the second and third parameters are listed in more detail below.
-        opts, args = getopt.getopt(argv, "ht:t2:", ["help", "test=", "test2="])
-    except getopt.GetoptError:
-        # Ran into an error while converting the user supplied options. User should check their arguments and rerun the application.
-        # Printing application usage and exiting.
-        logging.warning("Did not use offered parameters")
-        print(get_usage())
-        sys.exit(2)
-
-    # Stepping through each user supplied argument.
-    for opt, arg in opts:
-        if opt in ("-h", "--help"):
-            # User asked to print the help menu.
-            # Exit after printing help.
-            print(get_usage())
-            sys.exit()
-        elif opt in ("-t", "--test"):
-            # Using user provided audio file.
-            test_var = arg
-
-    ####################################
-    ### Main Code Section
-    ####################################
-
-    # Do something 1
-    print("I'm doing something 1.")
-    logging.info(" Doing something.")
-
-    # Do something 2
-    print("I'm doing something 2.")
-
-    print("Oh by the way, test var is: " + str(test_var))
-
-
-def get_usage():
-    ""
-    Prints the application usage information
-
-    :return: String that describes the application usage.
-    ""
-    usage_text = ""
-smash.py [opts]
-
-    -t value, --test=<value>:                         Test value
-    -h, --help:                                       Displays usage information.
-
-    ""
-
-    return usage_text"""
 
 if __name__ == "__main__":
     """
@@ -84,5 +35,5 @@ if __name__ == "__main__":
     Generally, this should be the last bit of code in this script.
     """
     logging.basicConfig(level=logging.DEBUG)
-    #main(sys.argv[1:])
     testVariable()
+    fileConvert()
