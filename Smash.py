52ecdc0f92b97754a81633429185b1ae9c783ba9
livia <liviag.earl@gmail.com>
2022-12-12 13:45:57 -0500

    Finished click options for both Smash.py and convert-to-hls.py

--- Smash.py
+++ Smash.py
@@ -6,27 +6,14 @@ import sys
 import click
 
 @click.command()
-@click.option("--test", "-t", default = 1, help = "Test Value")
-@click.option("--inFile", "-i", default = 'out.mp4', prompt = "Enter the name of input file", help = "mp4 video file inputted by the user")
-@click.option("--outFile", "-o", default = 'output.m3u8', prompt = "Enter the name of export file", help = "Name of the export file")
+@click.option("--lowres", "-l", default = 'low.m3u8', help = "Low resolution video in .m3u8")
+@click.option("--highres", "-h", default = 'high.m3u8', help = "High resolution video in .m3u8")
+@click.option("--message", "-m", default = 'Hello, World',  help = "Message to be hidden in file")
 
-def testVariable(test):
-    #for test variables
-    click.echo("I'm doing something 1. This is test variable: " + str(test))
+def main(lowres, highres, message):
+    #for actual steg process
+    click.echo("Files: " + highres + " and " + lowres + ". Message: " + message)
 
-def fileConvert(inFile, outFile):
-    #for file conversion
-    temp = inFile
-    temp2 = outFile
-    click.echo("Files: " + inFile + " and " + outFile)
-
-def main():
-    """
-    This function is the entry point for this application.
-
-    :param argv: List of command line arguments
-    """
-    # Program Variables
 
 if __name__ == "__main__":
     """
@@ -35,5 +22,4 @@ if __name__ == "__main__":
     Generally, this should be the last bit of code in this script.
     """
     logging.basicConfig(level=logging.DEBUG)
-    #testVariable()
-    fileConvert()
+    main()
--- convert-to-hls.py
+++ convert-to-hls.py
@@ -1,6 +1,32 @@
-
+import click
+import logging
 import ffmpeg
-file = input("Enter file: ")
-input_stream = ffmpeg.input(file, f='mp4')
-output_stream = ffmpeg.output(input_stream, 'output.m3u8', format='hls', start_number=0, hls_time=5, hls_list_size=0)
-ffmpeg.run(output_stream)
+
+@click.command()
+@click.option("--test", "-t", default = 1, help = "Test Value")
+@click.option("--infile", "-i", default = 'out.mp4', help = "mp4 video file inputted by the user")
+@click.option("--outfile", "-o", default = 'output.m3u8',  help = "Name of the export file")
+
+def main(test, infile, outfile):
+    #for file conversion
+    media_input_filename = infile
+    playlist_filename = outfile
+    input_stream = ffmpeg.input(media_input_filename, f='mp4')
+    output_stream = ffmpeg.output(input_stream, playlist_filename, format='hls', start_number=0, hls_time=5, hls_list_size=0)
+    ffmpeg.run(output_stream)
+    click.echo("Files: " + media_input_filename + " and " + playlist_filename)
+    testVariable(test)
+
+def testVariable(test_var):
+    #for test variables
+    print(f"My local test variable is: {test_var}")
+
+
+if __name__ == "__main__":
+    """
+    This initiates and calls the main function for this application. 
+
+    Generally, this should be the last bit of code in this script.
+    """
+    logging.basicConfig(level=logging.DEBUG)
+    main()
