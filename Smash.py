559025508202413e26c63b909cc2c50fd9996e2c
livia <liviag.earl@gmail.com>
2022-12-12 11:56:17 -0500

    Trying to solev errors

--- Smash.py
+++ Smash.py
@@ -7,17 +7,18 @@ import click
 
 @click.command()
 @click.option("--test", "-t", default = 1, help = "Test Value")
-@click.option("--inFile", "-i", default = 'out.mp4', help = "mp4 video file inputted by the user")
-@click.option("--outFile", "-o", default = 'output.m3u8', help = "Name of the export file")
+@click.option("--inFile", "-i", default = 'out.mp4', prompt = "Enter the name of input file", help = "mp4 video file inputted by the user")
+@click.option("--outFile", "-o", default = 'output.m3u8', prompt = "Enter the name of export file", help = "Name of the export file")
 
 def testVariable(test):
     #for test variables
-    test_var = test
-    click.echo("I'm doing something 1. This is test variable: " + str(test_var))
+    click.echo("I'm doing something 1. This is test variable: " + str(test))
 
 def fileConvert(inFile, outFile):
     #for file conversion
     temp = inFile
+    temp2 = outFile
+    click.echo("Files: " + inFile + " and " + outFile)
 
 def main():
     """
@@ -26,7 +27,6 @@ def main():
     :param argv: List of command line arguments
     """
     # Program Variables
-    test_var = 0
 
 if __name__ == "__main__":
     """
@@ -35,5 +35,5 @@ if __name__ == "__main__":
     Generally, this should be the last bit of code in this script.
     """
     logging.basicConfig(level=logging.DEBUG)
-    testVariable()
+    #testVariable()
     fileConvert()
