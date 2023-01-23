33aae62438176bec663cf51e0ae049b2dc5e3b41
liviaearl <liviag.earl@gmail.com>
2023-01-23 13:35:02 -0500

    updating all of the files after creating new project

--- Smash.py
+++ Smash.py
@@ -8,11 +8,15 @@ import click
 @click.command()
 @click.option("--lowres", "-l", default = 'low.m3u8', help = "Low resolution video in .m3u8")
 @click.option("--highres", "-h", default = 'high.m3u8', help = "High resolution video in .m3u8")
-@click.option("--message", "-m", default = 'Hello, World',  help = "Message to be hidden in file")
+@click.option("--textmessage", "-t", default = 'Hello, World',  help = "Text message to be hidden in file")
+@click.option("--filemessage", "-f", default = 'C:\\Users\\livia\\PycharmProjects\\SMASH\\message.idk',  help = "File message to be hidden in file")
 
-def main(lowres, highres, message):
+def main(lowres, highres, textmessage, filemessage):
     #for actual steg process
-    click.echo("Files: " + highres + " and " + lowres + ". Message: " + message)
+    if textmessage != "":
+        click.echo("Files: " + highres + " and " + lowres + ". Message: " + textmessage)
+    else:
+        click.echo("Files: " + highres + " and " + lowres + ". Message: " + filemessage)
 
 
 if __name__ == "__main__":
--- convert-to-hls.py
+++ convert-to-hls.py
@@ -3,17 +3,15 @@ import logging
 import ffmpeg
 import os
 
-#@click.option("--infile", "-i", default='C:\Users\livia\OneDrive\Documents\srt 4\ex.mp4', help="mp4 video file inputted by the user")
-#media_input_filename = os.listdir(infile)
 @click.command()
 @click.option("--test", "-t", default=1, help="Test Value")
-@click.option("--infile", "-i", default='ex.mp4', help="mp4 video file inputted by the user")
-@click.option("--outfile", "-o", default='output.m3u8', help="Name of the export file")
+@click.option("--infile", "-i", default='C:\\Users\\livia\\PycharmProjects\\SMASH\\audio\\tangled-tlr1080.mov', help="mp4 video file inputted by the user")
+@click.option("--outfile", "-o", default='C:\\Users\\livia\\PycharmProjects\\SMASH\\audio\\high\\outputh5.m3u8', help="Name of the export file")
 def main(test, infile, outfile):
     # for file conversion
     media_input_filename = infile
     playlist_filename = outfile
-    input_stream = ffmpeg.input(media_input_filename, f='mp4')
+    input_stream = ffmpeg.input(media_input_filename, f='mov')
     output_stream = ffmpeg.output(input_stream, playlist_filename, format='hls', start_number=0, hls_time=5,
                                   hls_list_size=0)
     ffmpeg.run(output_stream)
